ec8baaef2af0317a0d418ab06eb8122080fcf5b1
Karimifar <em.karimifar@gmail.com>
2020-01-07 16:31:37 -0600

    added all projects

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,29 +13,65 @@ console.log(parallaxEl)
 window.addEventListener("scroll", function(e){
     var scrollingDown = this.oldScroll < this.scrollY;
     this.oldScroll = this.scrollY;
-    if(scrollingDown){
-        for(var i=0; i<parallaxEl.length; i++){
-            var el = parallaxEl[i]
-            var nextEl = parallaxEl[i+1]
-            var prevEl = parallaxEl[i-1]
-            var viewportOffset = el.getBoundingClientRect();
-            // console.log(viewportOffset)
-            if (viewportOffset.top < 0 && viewportOffset.bottom >0){
-                console.log("inside "+i)
-                // console.log(el)
+    // if(scrollingDown){
+    //     for(var i=0; i<parallaxEl.length; i++){
+    //         var el = parallaxEl[i]
+    //         var nextEl = parallaxEl[i+1]
+    //         var prevEl = parallaxEl[i-1]
+    //         var viewportOffset = el.getBoundingClientRect();
+    //         // console.log(viewportOffset)
+    //         if (viewportOffset.top < 0 && viewportOffset.bottom >0){
+    //             console.log("inside "+i)
+    //             // console.log(el)
     
-                // $(prevEl).removeClass("isFixed")
-                $(el).addClass("isFixed")
-            }else{
-                console.log("else")
-                $(el).removeClass("isFixed")
-            }
-        }
-    }else{
-        for(var i=0; i<parallaxEl.length; i++){
+    //             // $(prevEl).removeClass("isFixed")
+    //             $(el).addClass("isFixed")
+    //         }else{
+    //             console.log("else")
+    //             $(el).removeClass("isFixed")
+    //         }
+    //     }
+    // }else{
+    //     for(var i=0; i<parallaxEl.length; i++){
             
-        }
+    //     }
 
-    }
+    // }
     
+})
+
+
+
+
+
+////observer test
+var projectImages = document.querySelectorAll('.project-img img');
+
+
+var options = {
+	rootMargin: '0px 0px -350px 0px',
+	threshold: 0.00001
+  };
+function handleIntersection(entries, observer){
+entries.forEach( function(entry){
+    // console.log(entry.intersectionRect.height)
+    var img = entry.target
+    var status = entry.target.dataset.status
+    console.log(entry.intersectionRatio)
+    if(entry.intersectionRatio > 0 && status === "flat") {
+        console.log(entry)
+        console.log(img)
+        $(img).css("transform","rotateY(-0deg)")
+        console.log($(img).attr("data-status"))
+        entry.target.dataset.status = "perspective"
+    }else if(status ==="perspective"){
+        $(img).css("transform","rotateY(-30deg)")
+        entry.target.dataset.status = "flat"
+    }
+})
+}
+var observer = new IntersectionObserver(handleIntersection, options);
+
+projectImages.forEach(function(img){
+  observer.observe(img);
 })
