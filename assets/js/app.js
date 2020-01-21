7b059c69a0b46b2d8b4c1eb560758d4ce513404c
Karimifar <em.karimifar@gmail.com>
2020-01-21 16:46:03 -0600

    scroll back works

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,9 +9,9 @@ console.log(parallaxEl)
 //         }
 //     }
 // })
-
+var scrollingDown
 window.addEventListener("scroll", function(e){
-    var scrollingDown = this.oldScroll < this.scrollY;
+    scrollingDown = this.oldScroll < this.scrollY;
     this.oldScroll = this.scrollY;
     // if(scrollingDown){
     //     for(var i=0; i<parallaxEl.length; i++){
@@ -50,22 +50,29 @@ var projects = document.querySelectorAll('.project-row');
 
 var options = {
   //window.innerHeight
-	rootMargin:   '0px 0px -' + (window.innerHeight-100) + 'px 0px',
-	threshold: 0.01
+	rootMargin:   '0px',// 0px -' + (window.innerHeight-100) + 'px 0px',
+	threshold: 0.5
   };
 function handleIntersection(entries, observer){
+  console.log("scroll down",scrollingDown)
 entries.forEach( function(entry){
     // console.log(entry.intersectionRect.height)
     var project = entry.target
-    var title = entry.target.dataset.title
+    var title = project.dataset.title
+    var index = project.dataset.index
     console.log(title)
     console.log(entry.intersectionRatio)
-    if(entry.intersectionRatio > 0 ) {
+    if(scrollingDown && entry.intersectionRatio > 0.50) {
         console.log(entry)
         console.log(project)
         $("#proj-title h1").text(title)
         // console.log($(img).attr("data-status"))
         // entry.target.dataset.status = "perspective"
+    }else if(!scrollingDown){
+      var prevTitle = projects[index-1].getAttribute("data-title");
+      console.log(prevTitle)
+      $("#proj-title h1").text(prevTitle)
+
     }
 })
 }
