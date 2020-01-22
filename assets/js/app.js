41853120b8d5edc1e659372d4196c312378e0f6d
Karimifar <em.karimifar@gmail.com>
2020-01-22 10:32:04 -0600

    link text

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,24 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
 var parallaxEl = document.getElementsByClassName("project-row")
-console.log(parallaxEl)
-// $(window).on('scroll', function(){
-//     for(var i=0; i<parallaxEl.length; i++){
-//         var el = parallaxEl[i];
-//         console.log($(el).scrollTop())
-//         if($(el).scrollTop()< 10){
-//             console.log("hit top")
-//         }
-//     }
-// })
+
+var parallaxWrap = document.getElementById("parallax-wrap")
+
 var scrollingDown
 window.addEventListener("scroll", function(e){
     scrollingDown = this.oldScroll < this.scrollY;
     this.oldScroll = this.scrollY;
-    // if(scrollingDown){
-    //     for(var i=0; i<parallaxEl.length; i++){
-    //         var el = parallaxEl[i]
-    //         var nextEl = parallaxEl[i+1]
-    //         var prevEl = parallaxEl[i-1]
-    //         var viewportOffset = el.getBoundingClientRect();
-    //         // console.log(viewportOffset)
-    //         if (viewportOffset.top < 0 && viewportOffset.bottom >0){
-    //             console.log("inside "+i)
-    //             // console.log(el)
-    
-    //             // $(prevEl).removeClass("isFixed")
-    //             $(el).addClass("isFixed")
-    //         }else{
-    //             console.log("else")
-    //             $(el).removeClass("isFixed")
-    //         }
-    //     }
-    // }else{
-    //     for(var i=0; i<parallaxEl.length; i++){
-            
-    //     }
 
-    // }
+    var viewportOffset = parallaxWrap.getBoundingClientRect();
+    if (viewportOffset.top < 0 && viewportOffset.bottom >0){
+        // console.log(el)
+        $("#proj-title").addClass("isFixed")
+        // $(prevEl).removeClass("isFixed")
+        // $(el).addClass("isFixed")
+    }else{
+        $("#proj-title").removeClass("isFixed")
+    }
     
 })
 
